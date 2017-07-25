723ae5e8dad908f9f152f97820ae01ce2891582e
wangyu <1837960253@qq.com>
2017-07-25 17:55:11 +0800

    App替换成kotlin

---
+++ app/src/main/java/com/jianyuyouhun/kotlin/kotlinjmvp/App.kt
@@ -0,0 +1,18 @@
+package com.jianyuyouhun.kotlin.kotlinjmvp
+
+import com.jianyuyouhun.kotlin.library.app.KTApp
+import com.jianyuyouhun.kotlin.library.mvp.BaseKTModel
+
+/**
+ *
+ * Created by wangyu on 2017/7/25.
+ */
+class App: KTApp() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+    override fun initModels(models: List<BaseKTModel>) {
+
+    }
+}
