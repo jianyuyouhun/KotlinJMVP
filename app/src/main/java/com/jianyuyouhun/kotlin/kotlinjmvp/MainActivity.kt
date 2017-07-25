335a730a637e07e0c84126d718116cb2bfe0e466
wangyu <1837960253@qq.com>
2017-07-25 17:57:35 +0800

    MainActivity修改

--- app/src/main/java/com/jianyuyouhun/kotlin/kotlinjmvp/MainActivity.kt
+++ app/src/main/java/com/jianyuyouhun/kotlin/kotlinjmvp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.jianyuyouhun.kotlin.kotlinjmvp
 
 import android.os.Bundle
+import android.widget.TextView
 import com.jianyuyouhun.kotlin.library.app.BaseActivity
 
 class MainActivity : BaseActivity() {
+
+    var textView: TextView? = null
+
     override fun getLayoutResId(): Int {
         return R.layout.activity_main
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        textView = findViewById(R.id.textView) as TextView
+        textView!!.text="hello Kotlin"
     }
 }
