56e7c8127beb81eb1aa1171ea8f9f2a574408506
wangyu <1837960253@qq.com>
2017-11-15 10:51:16 +0800

    修改ModelInjector

--- library/src/main/java/com/jianyuyouhun/kotlin/library/utils/injecter/ModelInjector.kt
+++ library/src/main/java/com/jianyuyouhun/kotlin/library/utils/injecter/ModelInjector.kt
@@ -12,28 +12,27 @@ fun injectModel(any: Any) {
     var cls: Class<*> = any::class.java
     while (cls != BaseKTModel::class.java && cls != Any::class.java) {
         val declaredFields = cls.declaredFields ?: continue
-        if (declaredFields.isNotEmpty()) {
-            for (field in declaredFields) {
-                val modifiers = field.modifiers
-                if (Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers)) continue
+        if (declaredFields.isEmpty()) continue
+        for (field in declaredFields) {
+            val modifiers = field.modifiers
+            if (Modifier.isFinal(modifiers) || Modifier.isStatic(modifiers)) continue
 
-                if (field.isAnnotationPresent(Model::class.java)) continue
+            if (field.isAnnotationPresent(Model::class.java)) continue
 
-                val type = field.type
-                if (!BaseKTModel::class.java.isAssignableFrom(type)) {
-                    throw ModelInjectException("@Model 只能在BaseJModel子类中使用")
-                }
+            val type = field.type
+            if (!BaseKTModel::class.java.isAssignableFrom(type)) {
+                throw ModelInjectException("@Model 只能在BaseJModel子类中使用")
+            }
 
-                @Suppress("UNCHECKED_CAST")
-                val baseModel = KTApp.mInstance.getKTModel(type as Class<out BaseKTModel>) ?: throw ModelInjectException(type.simpleName + "Model 还未初始化")
+            @Suppress("UNCHECKED_CAST")
+            val baseModel = KTApp.mInstance.getKTModel(type as Class<out BaseKTModel>) ?: throw ModelInjectException(type.simpleName + "Model 还未初始化")
 
-                if (!field.isAccessible) field.isAccessible = true
+            if (!field.isAccessible) field.isAccessible = true
 
-                try {
-                    field.set(any, baseModel)
-                } catch (e: IllegalAccessException) {
-                    e.printStackTrace()
-                }
+            try {
+                field.set(any, baseModel)
+            } catch (e: IllegalAccessException) {
+                e.printStackTrace()
             }
         }
         cls = cls::class.java.superclass
