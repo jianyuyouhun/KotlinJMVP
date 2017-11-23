a001972233baeaf761b02bbb387014733948cd97
wangyu <1837960253@qq.com>
2017-11-23 11:02:11 +0800

    增加版本兼容执行判断代码

---
+++ library/src/main/java/com/jianyuyouhun/kotlin/library/utils/VersionCompatibility.kt
@@ -0,0 +1,34 @@
+package com.jianyuyouhun.kotlin.library.utils
+
+import android.os.Build
+
+/**
+ * 版本兼容执行环境
+ * Created by wangyu on 2017/11/23.
+ */
+
+inline fun onTargetOrAbove(target: Int, up: () -> Unit, down: () -> Unit) {
+    if (Build.VERSION.SDK_INT >= target) {
+        up()
+    } else {
+        down()
+    }
+}
+
+internal inline fun on14orAbove(up: () -> Unit, down: () -> Unit)
+    = onTargetOrAbove(Build.VERSION_CODES.ICE_CREAM_SANDWICH, up, down)
+
+internal inline fun on17orAbove(up: () -> Unit, down: () -> Unit)
+    = onTargetOrAbove(Build.VERSION_CODES.JELLY_BEAN_MR1, up, down)
+
+internal inline fun on19orAbove(up: () -> Unit, down: () -> Unit)
+    = onTargetOrAbove(Build.VERSION_CODES.KITKAT, up, down)
+
+internal inline fun on21orAbove(up: () -> Unit, down: () -> Unit)
+    = onTargetOrAbove(Build.VERSION_CODES.LOLLIPOP, up, down)
+
+internal inline fun on23orAbove(up: () -> Unit, down: () -> Unit)
+    = onTargetOrAbove(Build.VERSION_CODES.M, up, down)
+
+internal inline fun on24orAbove(up: () -> Unit, down: () -> Unit)
+    = onTargetOrAbove(Build.VERSION_CODES.N, up, down)
